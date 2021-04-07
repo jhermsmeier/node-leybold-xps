32fc080bd9dc0351886245d1ba619029a52a1911
Jonas Hermsmeier <jhermsmeier@gmail.com>
2021-04-07 12:56:04 +0200

    test: Structure tests, add assertions for region data

--- test/index.js
+++ test/index.js
@@ -3,9 +3,31 @@ var path = require( 'path' )
 var assert = require( 'assert' )
 var leybold = require( '..' )
 
-var filename = path.join( __dirname, 'data', 'Leybold.dat' )
-var contents = fs.readFileSync( filename, 'utf8' )
-var data = leybold.decode( contents )
-var encoded = leybold.encode( data )
+context( 'Leybold', () => {
 
-assert.equal( encoded, contents )
+  var filename = path.join( __dirname, 'data', 'Leybold.dat' )
+  var contents = fs.readFileSync( filename, 'utf8' )
+
+  test( 'decode()', () => {
+
+    var data = leybold.decode( contents )
+
+    assert.equal( data.regions.length, 4 )
+
+    assert.equal( data.regions[0].data.length, 241 )
+    assert.equal( data.regions[1].data.length, 381 )
+    assert.equal( data.regions[2].data.length, 641 )
+    assert.equal( data.regions[3].data.length, 247 )
+
+  })
+
+  test( 'encode()', () => {
+
+    var data = leybold.decode( contents )
+    var encoded = leybold.encode( data )
+
+    assert.equal( encoded, contents )
+
+  })
+
+})
